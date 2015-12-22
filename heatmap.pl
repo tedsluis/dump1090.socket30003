c96225693274d28608b1dad6def4f9a424a64d02
tedsluis <ted.sluis@gmail.com>
2015-12-22 21:35:01 +0100

    Added reading parameters from config file and writing messages to common log routine.

--- heatmap.pl
+++ heatmap.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# Ted Sluis 2015-09-06-24
+# Ted Sluis 2015-22-12-24
 # heatmap.pl
 #
 #===============================================================================
@@ -18,34 +18,22 @@ BEGIN {
 }
 #
 #===============================================================================
+my $logfile ="";
+#===============================================================================
 # Ctrl-C interupt handler
 $SIG{'INT'} = \&intHandler;
 
 sub intHandler {
 	# Someone pressed Ctrl-C
-	print "\nCTRL-C was pressed. Do you want to exit '$scriptname'? (y/n)\n";
+	LOG("CTRL-C was pressed. Do you want to exit '$scriptname'? (y/n)","W");
 	my $answer = <STDIN>;
 	if ($answer =~ /^y$/i) {
-		print "Exiting '$scriptname'.....\n";
+		LOG("Exiting '$scriptname'.....","I");
 		exit 1;
 	} else {
-		print "'$scriptname' is continuing.......\n";
+		LOG("'$scriptname' is continuing.......","I");
 	}
 }
-#===============================================================================
-# Read settings from config file
-my %setting = common->READCONFIG('socket30003.cfg',$fullscriptname);
-# Use parameterS & values from the 'heatmap' section. If empty or not-exists, then use from the 'common' section, otherwise script defaults.
-my $default_datadirectory = $setting{'heatmap'}{'defaultdatadirectory'} || $setting{'common'}{'defaultdatadirectory'} || "/tmp";
-my $outputfile            = $setting{'heatmap'}{'outputfile'}           || $setting{'common'}{'outputfile'}           || "heatmapcode.csv";
-my $outputdatafile	  = $setting{'heatmap'}{'outputdatafile'}       || $setting{'common'}{'outputdatafile'}       || "heatmapdata.csv";
-my $latitude              = $setting{'heatmap'}{'latitude'}             || $setting{'common'}{'latitude'}             || 52.085624; # Antenna location
-my $longitude             = $setting{'heatmap'}{'longitude'}            || $setting{'common'}{'longitude'}            || 5.0890591; # 
-my $degrees               = $setting{'heatmap'}{'degrees'}              || 5;        # used to determine boundary of area around antenne.
-my $resolution            = $setting{'heatmap'}{'resolution'}           || 1000;     # number of horizontal and vertical positions in output file.
-my $max_positions         = $setting{'heatmap'}{'max_positions'}        || 100000;   # maximum number of positions in the outputfile.
-my $max_weight            = $setting{'heatmap'}{'max_weight'}           || 1000;     # maximum position weight on the heatmap.
-#
 #===============================================================================
 # Get options
 my $help;
@@ -53,6 +41,12 @@ my $datadirectory;
 my $filemask;
 my $lon;
 my $lat;
+my $max_positions;
+my $resolution;
+my $degrees;
+my $max_weight;
+my $debug = 0;
+my $verbose = 0;
 
 GetOptions(
 	"help!"=>\$help,
@@ -64,7 +58,40 @@ GetOptions(
 	"resolution=s"=>\$resolution,
 	"degrees=s"=>\$degrees,
 	"maxweight=s"=>\$max_weight,
+        "debug!"=>\$debug,
+        "verbose!"=>\$verbose
 ) or exit(1);
+#
+#===============================================================================
+# if '-debug' parameter is used, set debug mode:
+common->setdebug if ($debug);
+#
+#===============================================================================
+# if '-verbose' parameter is used, set verbose mode:
+common->LOGverbose if ($verbose);
+#
+#===============================================================================
+# Checks if script runs interactive.
+my $interactive = common->InteractiveShellCheck;
+#
+#===============================================================================
+# Log routine
+sub LOG(@){
+        common->LOG($logfile,@_);
+}
+#
+#===============================================================================
+# Read settings from config file
+my %setting = common->READCONFIG('socket30003.cfg',$fullscriptname);
+# Use parameters & values from the 'heatmap' section. If empty or not-exists, then use from the 'common' section, otherwise script defaults.
+my $default_datadirectory = $setting{'heatmap'}{'defaultdatadirectory'} || $setting{'common'}{'defaultdatadirectory'} || "/tmp";
+my $outputdatafile	  = $setting{'heatmap'}{'outputdatafile'}       || $setting{'common'}{'outputdatafile'}       || "heatmapdata.csv";
+my $latitude              = $setting{'heatmap'}{'latitude'}             || $setting{'common'}{'latitude'}             || 52.085624; # Antenna location
+my $longitude             = $setting{'heatmap'}{'longitude'}            || $setting{'common'}{'longitude'}            || 5.0890591; # 
+   $degrees               = $degrees       || $setting{'heatmap'}{'degrees'}      || 5;        # used to determine boundary of area around antenne.
+   $resolution            = $resolution    || $setting{'heatmap'}{'resolution'}   || 1000;     # number of horizontal and vertical positions in output file.
+   $max_positions         = $max_positions || $setting{'heatmap'}{'max_positions'}|| 100000;   # maximum number of positions in the outputfile.
+   $max_weight            = $max_weight    || $setting{'heatmap'}{'max_weight'}   || 1000;     # maximum position weight on the heatmap.
 #
 #===============================================================================
 # Check options:
@@ -72,11 +99,8 @@ if ($help) {
 	print "\nThis $scriptname script creates heatmap data 
 which can be displated in a modified variant of dump1090-mutobility.
 
-It creates two output files:
-1) One file with locations in java script code, which must be added
-   to the script.js manualy.
-2) One file with location data in csv format, which can be imported
-   from the dump1090 GUI.
+It create a output file with location data in csv format, which can 
+be imported using the dump1090 GUI.
 
 Please read this post for more info:
 http://discussions.flightaware.com/post180185.html#p180185
@@ -124,10 +148,13 @@ Optional parameters:
 	-degrees <number>               To determine boundaries of area around the antenna.
 	                                (lat-degree <--> lat+degree) x (lon-degree <--> lon+degree)
 	                                De default is 5 degree.
+
+        -debug                          Displays raw socket messages.
+        -verbose                        Displays verbose log messages.
 	-help				This help page.
 
 note: 
-	The default values can be changed within the script (in the most upper section).
+         The default values can be changed within the config file 'socket30003.cfg'.
 
 
 Examples:
@@ -140,8 +167,8 @@ Examples:
 # Resolution, Degrees & Factor
 if ($resolution) {
 	if ($resolution !~ /^\d{2,5}$/) {
-                print "The resolution '$resolution' is invalid!\n";
-                print "It should be between 10 and 99999.\n";
+                LOG("The resolution '$resolution' is invalid!","E");
+                LOG("It should be between 10 and 99999.","E");
                 exit;
         }
 } else {
@@ -149,8 +176,8 @@ if ($resolution) {
 }
 if ($degrees) {
         if ($degrees !~ /^\d{1,2}(\.\d{1,4})?$/) {
-                print "The given number of degrees '$degrees' is invalid!\n";
-                print "It should be between 0.0001 and 99.9999 degrees.\n";
+                LOG("The given number of degrees '$degrees' is invalid!","E");
+                LOG("It should be between 0.0001 and 99.9999 degrees.","E");
                 exit;
         }
 } else {
@@ -161,30 +188,30 @@ my $factor = int($resolution / ($degrees * 2));
 # Max positions
 if ($max_positions) {
 	if (($max_positions !~ /^\d{3,6}$/) && ($max_positions > 99) && ($max_positions < 1000000)) {
-		print "The maximum number of positions '$max_positions' is invalid!\n";
-		print "It should be between 100 and 999999.\n";
+		LOG("The maximum number of positions '$max_positions' is invalid!","E");
+		LOG("It should be between 100 and 999999.","E");
 		exit;
 	} 
 } else {
 	$max_positions = 100000;
 }
-print "There will be no more then '$max_positions' positions in the output file.\n";
+LOG("There will be no more then '$max_positions' positions in the output file.","I");
 #===============================================================================
 if ($max_weight) {
         if (($max_weight !~ /^\d{2,4}$/) && ($max_weight > 9) && ($max_weight < 10000)) {
-                print "The maximum position weight '$max_weight' is invalid!\n";
-                print "It should be between 10 and 9999.\n";
+                LOG("The maximum position weight '$max_weight' is invalid!","E");
+                LOG("It should be between 10 and 9999.","E");
                 exit;
         }
 } else {
         $max_weight = 1000;
 }
-print "The maximum position weight on the heatmap will be not more then '$max_weight'.\n";
+LOG("The maximum position weight on the heatmap will be not more then '$max_weight'.","I");
 #=============================================================================== 
 # Are the specified directories for data, log and pid file writeable?
 $datadirectory = $default_datadirectory if (!$datadirectory);
 if (!-w $datadirectory) {
-	print "The directory does not exists or you have no write permissions in data directory '$datadirectory'!\n";
+	LOG("The directory does not exists or you have no write permissions in data directory '$datadirectory'!","E");
 	exit 1;
 }
 #===============================================================================
@@ -192,13 +219,13 @@ if (!-w $datadirectory) {
 $longitude = $lon if ($lon);
 $longitude =~ s/,/\./ if ($longitude);
 if ($longitude !~ /^[-+]?\d+(\.\d+)?$/) {
-	print "The specified longitude '$longitude' is invalid!\n";
+	LOG("The specified longitude '$longitude' is invalid!","E");
 	exit 1;
 }
 $latitude = $lat if ($lat);
 $latitude  =~ s/,/\./ if ($latitude);
 if ($latitude !~ /^[-+]?\d+(\.\d+)?$/) {
-	print"The specified latitude '$latitude' is invalid!\n";
+	LOG("The specified latitude '$latitude' is invalid!","E");
 	exit 1;
 }
 # area around antenna
@@ -208,9 +235,9 @@ my $lat1 = int(($latitude  - $degrees) * 10) / 10; # most westerly latitude
 my $lat2 = int(($latitude  + $degrees) * 10) / 10; # most easterly latitude
 my $lon1 = int(($longitude - $degrees) * 10) / 10; # most northerly longitude
 my $lon2 = int(($longitude + $degrees) * 10) / 10; # most southerly longitude
-print "The resolution op the heatmap will be ${resolution}x${resolution}.\n";
-print "The antenna latitude & longitude are: '$latitude','$longitude'.\n";
-print "The heatmap will cover the area of $degrees degree around the antenna, which is between latitude $lat1 - $lat2 and longitude $lon1 - $lon2.\n";
+LOG("The resolution op the heatmap will be ${resolution}x${resolution}.","I");
+LOG("The antenna latitude & longitude are: '$latitude','$longitude'.","I");
+LOG("The heatmap will cover the area of $degrees degree around the antenna, which is between latitude $lat1 - $lat2 and longitude $lon1 - $lon2.","I");
 #===============================================================================
 my %data;
 # Set default filemask
@@ -222,28 +249,26 @@ if (!$filemask) {
 # Find files
 my @files =`find $datadirectory -name $filemask`;
 if (@files == 0) {
-	print "No files were found in '$datadirectory' that matches with the '$filemask' filemask!\n";
+	LOG("No files were found in '$datadirectory' that matches with the '$filemask' filemask!","E");
 	exit 1;
 } else {
-	print "The following files fit with the filemask '$filemask':\n";
+	LOG("The following files fit with the filemask '$filemask':","I");
 	my @tmp;
 	foreach my $file (@files) {
 		chomp($file);
 		next if ($file =~ /log$|pid$/i);
-		print "  $file\n";
+		LOG("  $file","I");
 		push(@tmp,$file);
 	}
 	@files = @tmp;
 	if (@files == 0) {
-        	print "No files were found in '$datadirectory' that matches with the '$filemask' filemask!\n";
+        	LOG("No files were found in '$datadirectory' that matches with the '$filemask' filemask!","E");
         	exit 1;
 	}
 }
 #===============================================================================
 my %pos;
-$outputfile = "$datadirectory/$outputfile";
 $outputdatafile = "$datadirectory/$outputdatafile";
-open(my $output, '>', "$outputfile") or die "Could not open file '$outputfile' $!";
 open(my $outputdata, '>', "$outputdatafile") or die "Could not open file '$outputdatafile' $!";
 print $outputdata "\"weight\";\"lat\";\"lon\"";
 # Read input files
@@ -251,18 +276,22 @@ foreach my $filename (@files) {
 	chomp($filename);
 	# Read data file
 	open(my $data_filehandle, '<', $filename) or die "Could not open file '$filename' $!";
-	print "Processing file '$filename':\n";
+	LOG("Processing file '$filename':","I");
 	my $outside_area = 0;
 	my $linecounter = 0;
         my @header;
         my %hdr;
+	my $message="";
 	while (my $line = <$data_filehandle>) {
 		chomp($line);
 		$linecounter++;
                 # Data Header
                 # First line? 
                 if (($linecounter == 1) || ($line =~ /hex_ident/)){
-			print "- ".($linecounter-1)." processed.\n" if ($linecounter != 1);
+			if ($linecounter != 1) {
+				$message  .= "- ".($linecounter-1)." processed.";
+				LOG($message,"I");
+			}
                 	# Reset fileunit:
                         #%fileunit =();
                         # Does it contain header columns?
@@ -281,11 +310,11 @@ foreach my $filename (@files) {
                                 		push(@header,$column);
                                 	}
                       		}
-                        	print "  -header units:".join(",",@unit).", position $linecounter";
+                        	$message ="  -header units:".join(",",@unit).", position $linecounter";
                       	} else {
                         	# No header columns found. Use default!
                                 @header = ("hex_ident","altitude","latitude","longitude","date","time","angle","distance");
-				print "  -default units, position $linecounter";
+				$message ="  -default units, position $linecounter";
                         }
                        	# The file header unit information may be changed: set the units again.
                         #setunits;
@@ -312,7 +341,7 @@ foreach my $filename (@files) {
 		$pos{$lat}{$lon} = 0 if (!exists $pos{$lat}{$lon} );
 		$pos{$lat}{$lon} += 1;
 	}
-	print "-".($linecounter-1)." processed. $outside_area positions were out side the specified area.\n";
+	LOG($message."-".($linecounter-1)." processed. $outside_area positions were out side the specified area.","I");
 	close($data_filehandle);
 }
 # Sort positions based on the number of times they occured in the flight position data.
@@ -324,9 +353,9 @@ foreach my $lat (keys %pos) {
 		$sort{"$number,$lat,$lon"} = 1;
 	}
 }
-print "Number of sorted positions: ".(keys %sort)."\n";
+LOG("Number of sorted heatmap positions: ".(keys %sort),"I");
 # Get the highest :
-my ($highest_weight,@rubbishi)= reverse sort keys %sort;
+my ($highest_weight,@rubbish)= reverse sort keys %sort;
 $highest_weight =~ s/,.+,.+$//;
 # Get lowest weight:
 my $counter = 0;
@@ -340,12 +369,12 @@ foreach my $sort (reverse sort keys %sort) {
 		last;
 	}
 }
-print "The highest weight is '$highest_weight' and the lowest weight is '$lowest_weight'.\n";
+LOG("The highest weight is '$highest_weight' and the lowest weight is '$lowest_weight'.","I");
 # Is the highest weight more then the maximum weight?
 if ($max_weight > $highest_weight){
 	$max_weight = $highest_weight;
 } else {
-	print "Since the highest weight is more the the max weight '$max_weight' the weight of all points will be multiplied with a factor ".($max_weight / $highest_weight).".\n";
+	LOG("Since the highest weight is more the the max weight '$max_weight' the weight of all points will be multiplied with a factor ".($max_weight / $highest_weight).".","I");
 }
 # Proces the positions. Start with the positions that most occured in the flight position data.
 $counter = 0;
@@ -357,22 +386,7 @@ foreach my $sort (reverse sort keys %sort) {
 	# stop after the maximum number of heatmap positions is reached:
 	last if ($counter >= $max_positions);
 	# print output to file:
-	print $output "{location: new google.maps.LatLng($lat, $lon), weight: $weight},\n";
 	print $outputdata "\n\"$weight\";\"$lat\";\"$lon\"";
 }
-close($output);
 close($outputdata);
-# print a summery of the result:
-print "\nOutput file with java script code: $outputfile\n";
-my @cmd = `head -n 5 $outputfile`;
-print join("",@cmd);
-print "\n$counter rows with heatmap position data processed!\n\n";
-@cmd = `tail -n 5 $outputfile`;
-print join("",@cmd);
-#
-print "\nOutput file in csv format: $outputdatafile\n";
-@cmd = `head -n 5 $outputdatafile`;
-print join("",@cmd);
-print "\n$counter rows with heatmap position data processed!\n\n";
-@cmd = `tail -n 5 $outputdatafile`;
-print join("",@cmd)."\n";
+LOG("$counter rows with heatmap position data processed!","I");
